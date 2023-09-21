bb283261e1bd7d23e541420d5c0d978b4e3601b8
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-09-21 14:13:24 +0200

    thing

--- Plugin.cs
+++ Plugin.cs
@@ -1,47 +1,47 @@
 ﻿namespace DoctorBuffed
 {
-     using Exiled.API.Features;
-     using Exiled.API.Enums;
-     using DoctorBuffed.Events;
-     using MEC;
+    using Exiled.API.Features;
+    using Exiled.API.Enums;
+    using DoctorBuffed.Events;
+    using MEC;
 
-     public class Plugin : Plugin<Config>
-     {
-          public override string Name => "Scp049Buff";
-          public override string Prefix => "DoctorBuff";
-          public override string Author => "@misfiy";
-          public override PluginPriority Priority => PluginPriority.Default;
-          private Handler handler;
-          public static Plugin Instance;
-          private Config config;
-          public override void OnEnabled()
-          {
-               Instance = this;
-               config = Instance.Config;
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Scp049Buff";
+        public override string Prefix => "DoctorBuff";
+        public override string Author => "@misfiy";
+        public override Version Version => new(1, 1, 2);
+        public override Version RequiredExiledVersion => new(8, 2, 1);
 
-               RegisterEvents();
-               base.OnEnabled();
-          }
+        private Handler handler;
+        public static Plugin Instance;
+        public override void OnEnabled()
+        {
+            Instance = this;
 
-          public override void OnDisabled()
-          {
-               UnregisterEvents();
-               Instance = null!;
-               base.OnDisabled();
-          }
-          public void RegisterEvents()
-          {
-               handler = new Handler();
-               Exiled.Events.Handlers.Scp049.FinishingRecall += handler.OnFinishingRecall;
-               Exiled.Events.Handlers.Server.WaitingForPlayers += handler.OnWaitingForPlayers;
-               Log.Debug("Events have been registered!");
-          }
-          public void UnregisterEvents()
-          {
-               Exiled.Events.Handlers.Scp049.FinishingRecall -= handler.OnFinishingRecall;
-               Exiled.Events.Handlers.Server.WaitingForPlayers -= handler.OnWaitingForPlayers;
-               handler = null!;
-               Timing.KillCoroutines();
-          }
-     }
+            RegisterEvents();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+            Instance = null!;
+            base.OnDisabled();
+        }
+        public void RegisterEvents()
+        {
+            handler = new Handler();
+            Exiled.Events.Handlers.Scp049.FinishingRecall += handler.OnFinishingRecall;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += handler.OnWaitingForPlayers;
+            Log.Debug("Events have been registered!");
+        }
+        public void UnregisterEvents()
+        {
+            Exiled.Events.Handlers.Scp049.FinishingRecall -= handler.OnFinishingRecall;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= handler.OnWaitingForPlayers;
+            handler = null!;
+            Timing.KillCoroutines();
+        }
+    }
 }
